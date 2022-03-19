fc691a1e52a6a338d722192b7d9d150865724460
Braulio Dominguez <44845854+brolisdom@users.noreply.github.com>
2022-03-19 11:58:39 -0600

    Add files via upload

--- practica5/ClienteRMI.java
+++ practica5/ClienteRMI.java
@@ -3,15 +3,15 @@ import java.rmi.Naming;
 public class ClienteRMI {
     public static void main(String args[]) throws Exception {
         String url = "";
-        url = "rmi://20.228.196.65:50000/prueba";
+        url = "rmi://20.228.196.146:50000/prueba";
         InterfaceRMI nodo1 = (InterfaceRMI)Naming.lookup(url);
         System.out.println(nodo1.mayusculas("hola"));
 
-        url = "rmi://20.228.196.65:50000/prueba";
+        url = "rmi://20.228.196.146:50000/prueba";
         InterfaceRMI nodo2 = (InterfaceRMI)Naming.lookup(url);
         System.out.println("suma="+nodo2.suma(10,20));
 
-        url = "rmi://20.228.196.65:50000/prueba";
+        url = "rmi://20.228.196.146:50000/prueba";
         InterfaceRMI nodo3 = (InterfaceRMI)Naming.lookup(url);
         int [][] m = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         System.out.println("checksum="+nodo3.checksum(m));
--- practica5/ServidorRMI.java
+++ practica5/ServidorRMI.java
@@ -4,7 +4,7 @@ import java.rmi.registry.LocateRegistry;
 public class ServidorRMI {
     public static void main (String[] args) throws Exception {
         LocateRegistry.createRegistry(50000);
-        String url = "rmi://20.228.196.65:50000/prueba";
+        String url = "rmi://20.228.196.146:50000/prueba";
         ClaseRMI obj = new ClaseRMI();
         Naming.rebind(url, obj);
     }
